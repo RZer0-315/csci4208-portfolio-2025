bb405e6c6161af6c92bb35656f7b2fef8625b526
RZer0-315 <robertopina315@gmail.com>
2025-10-15 10:12:16 -0500

    finished Lab 6

--- labs/lab-06/scripts/app.js
+++ labs/lab-06/scripts/app.js
@@ -1,17 +1,87 @@
-import * as http from './http.js'           //Import http functions
-import * as view from './view.js'           //Import view functions
+import * as http from "./http.js"; //Import http functions
+import * as view from "./view.js"; //Import view functions
+
 const GET_TRIVIA = `https://opentdb.com/api.php?amount=1&difficulty=easy`; //Trivia GET endpoint
-const state = {};                           //Game state
+const BIN_ID = "68ef0778d0ea881f40a375c2"; //replace with your own
+const GET_LEADERBOARD = `https://api.jsonbin.io/v3/b/${BIN_ID}/latest`;
+const PUT_LEADERBOARD = `https://api.jsonbin.io/v3/b/${BIN_ID}`;
 
-const playGame = async () => {                      //PLAY function
-    const json = await http.sendGETRequest(GET_TRIVIA); //GET Request for trivia data
-    console.log(json);                                  //Print trivia data
-    [ state.trivia ] = json.results;
-    view.PlayScene(state);
+const state = {
+  score: 0,
+  timer: 20,
+  intervalId: null,
+  trivia: null,
+  topScores: [],
+}; //Game state
+
+const countdown = () => {
+  //COUNTDOWN function
+  if (state.timer > 0) {
+    //check if time remains
+    state.timer--; //decrement timer
+    view.PlayScene(state); //view render play scene
+  } else {
+    //when timer is 0
+    clearInterval(state.intervalId); //stop countdown interval
+    view.GameOverScene(state); //show gameover view
+  }
+};
+
+const getTop5 = async (newScore) => {
+const leaderboardJSON = await http.sendGETRequest(GET_LEADERBOARD);
+const top5 = leaderboardJSON.record;
+top5.push( newScore );
+top5.sort( (a,b) => b.score - a.score );
+top5.pop();
+return top5
 }
 
-window.start = async () => {            //START function
-    playGame();                         //call play function
+window.playGame = async () => {
+  //PLAY function
+  const json = await http.sendGETRequest(GET_TRIVIA); //GET Request for trivia data
+  console.log(json); //Print trivia data
+  [state.trivia] = json.results;
+  view.PlayScene(state);
+};
+
+window.createGame = () => {
+  //CREATE function
+  state.timer = 20; //set timer
+  state.intervalId = setInterval(countdown, 1000); //set interval id
+  playGame(); //call PLAY function
+};
+
+window.checkAnswer = (attempt) => {
+  //CHECK_ANSWER function
+  const answer = state.trivia.correct_answer; //Dereference answer
+  if (attempt == answer) {
+    //When Attempt is correct
+    state.score += state.timer; //Add to Score based on time
+    state.timer += 10; //Add 10 bonus seconds
+    playGame(); //Play Next Round of Trivia
+  } else {
+    //When Attempt is incorrect
+    clearInterval(state.intervalId); //stop countdown interval
+    view.GameOverScene(state); //show gameover view
+  }
+};
+
+window.start = async () => {
+  //START function
+  const leaderboardJSON = await http.sendGETRequest(GET_LEADERBOARD); //Fetch LeaderBoard
+  state.topScores = leaderboardJSON.record; //data in record prop
+  console.log(state.topScores); //Print LeaderBoard
+  state.score = 0; //reset score
+  state.timer = 20; //reset timer
+  view.StartMenu(state); //render Start Menu
+};
+
+window.updateLeaderboard = async () => {
+const name = document.getElementById('name').value;
+const currentScore = {name:name, score: state.score};
+const top5 = await getTop5(currentScore);
+await http.sendPUTRequest(PUT_LEADERBOARD, top5);
+start();
 }
 
-window.addEventListener('load', start); //When window loads execute start
+window.addEventListener("load", start); //When window loads execute start
--- labs/lab-06/scripts/view.js
+++ labs/lab-06/scripts/view.js
@@ -1,10 +1,41 @@
-import Question from './components/Question.js';       //Import Westion function
+import Question from "./components/Question.js"; //Import Westion function
+import HUD from "./components/HUD.js";
+import Skip from "./components/Skip.js";
+import Leaderboard from "./components/Leaderboard.js";
+import LeaderMenu from "./components/LeaderMenu.js";
 
-const renderDOM = (html) => document.getElementById('view').innerHTML = html;   //Set HTML in view
+const renderDOM = (html) => (document.getElementById("view").innerHTML = html); //Set HTML in view
+const isTop5 = (score, top5) => top5.some( item => item.score < score );
 
-export const PlayScene = (props) => { //Function for HTML view
-    const {trivia} = props; //Destructure properties
-    renderDOM( //render the Scene's HTML to DOM
-        `${Question(trivia)}`
-    )
-}
+export const PlayScene = (props) => {
+  //Function for HTML view
+  const { timer, score, trivia } = props; //Destructure properties
+  renderDOM(
+    //render the Scene's HTML to DOM
+    `${HUD(timer, score)}
+        ${Question(trivia)}
+        ${Skip()}`
+  );
+};
+
+export const GameOverScene = (props) => {
+  //Function for HTML view
+  const { timer, score, topScores } = props; //Destructure properties
+  renderDOM(
+    //render the Gameover HTML to DOM
+    `${HUD(timer, score)}
+    ${ isTop5( score, topScores) ? LeaderMenu() : '' }
+    <h1>Game Over!</h1>
+    <button onclick='start()'>Start Menu</button>`
+  );
+};
+
+export const StartMenu = (props) => {
+  const { timer, score, topScores } = props;
+  renderDOM(
+    `${HUD(timer, score)}
+    ${ Leaderboard(topScores)}
+    <hr>
+    <button onclick='createGame()'>Play</button>`
+  );
+};
